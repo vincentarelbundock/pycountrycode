b2ea42498974605d7347276903c465a4c236663a
Vincent Arel-Bundock <vincent.arel-bundock@umontreal.ca>
2023-06-21 17:33:19 -0400

    still broke

--- countrycode/countrycode.py
+++ countrycode/countrycode.py
@@ -11,32 +11,39 @@ with open(data_path, 'r') as f:
     data = [(x[0], x[1:]) for x in data]
     data = dict(data)
 
-def countrycode(sourcevar=['DZA', 'CAN'], origin='iso3c', destination='country.name.en', origin_regex = False):
+def countrycode(sourcevar=['DZA', 'CAN'], origin='iso3c', destination='country.name.en'):
 
     # sanity checks
+    if not isinstance(origin, str):
+        raise TypeError(f"Expected a string, got {type(argument).__name__}")
+    else:
+        if origin in ['country.name.en.regex', 'country.name.fr.regex', 'country.name.de.regex', 'country.name.it.regex']:
+            origin_regex = True
+        else:
+            origin_regex = False
+    if not isinstance(destination, str):
+        raise TypeError(f"Expected a string, got {type(destination).__name__}")
     if origin not in data.keys():
         raise ValueError(f"origin {origin} not found in the code list.")
     else:
         origin = data[origin]
-
     if destination not in data.keys():
         raise ValueError(f"destination {destination} not found in the code list.")
     else:
         destination = data[destination]
 
 
+    # we want to operate on lists, but allow and return single values
+    loner = False
     if isinstance(sourcevar, str):
         sourcevar = [sourcevar]
         loner = True
-    else:
-        loner = False
 
-    mapping = dict(zip(origin, destination))
 
     if origin_regex:
-        out = replace_regex(sourcevar, mapping)
+        out = replace_regex(sourcevar, origin, destination)
     else:
-        out = replace_exact(sourcevar, mapping)
+        out = replace_exact(sourcevar, origin, destination)
 
     # TODO: sanitize input
     # white space only if string
@@ -50,20 +57,22 @@ def countrycode(sourcevar=['DZA', 'CAN'], origin='iso3c', destination='country.n
     return out
 
 
-def replace_exact(sourcevar, mapping):
+def get_first_match(pattern, string_list):
+    for string in string_list:
+        match = pattern.search(string)
+        if match:
+            return match.group()
+    return None
+
+
+def replace_regex(sourcevar, origin, destination):
+    origin_compiled = [re.compile(x, flags=re.IGNORECASE) for x in origin]
+    origin_replaced = [get_first_match(x, sourcevar) for x in origin_compiled]
+    out = [v if v is not None else None for i, v in enumerate(origin_replaced)]
+    return out
+
+
+def replace_exact(sourcevar, origin, destination):
+    mapping = dict(zip(origin, destination))
     result = [mapping.get(item, None) for item in sourcevar]
-    return result
-
-def replace_regex(sourcevar, mapping):
-    result = []
-    for item in sourcevar:
-        replaced = False
-        for pattern, replacement in mapping.items():
-            if re.search(pattern, item):
-                result.append(re.sub(pattern, replacement, item))
-                replaced = True
-                break
-        if not replaced:
-            result.append(None)
-
     return result
--- trash.py
+++ trash.py
@@ -1,5 +1,34 @@
-from countrycode.countrycode import countrycode
+from countrycode.countrycode import *
+k = countrycode(['canada', 'united states', 'alGeria'], origin = "country.name.en.regex", destination = "iso3c")
 
-countrycode(['DZA', 'CAN', 'USA'], origin = "iso3c", destination = "cldr.short.en")
+gg
 
-countrycode(['Canada', 'United States'], origin = "country.name.en.regex", destination = "iso3c", origin_regex = True)
+countrycode(['DZA', 'CAN', 'USA'], origin = "iso3c", destination = "cldr.short.fr")
+countrycode(['DZA', 'CAN', 'USA'], origin = "iso3c", destination = "iso3n")
+
+# TODO: numeric codes should not be strings
+# probably due to my dict reading
+countrycode(['12', '124'], origin = "iso3n", destination = "cldr.short.fr")
+
+
+origin = [
+'zambia|northern.?rhodesia',
+'^(?!south)(?!republic).*viet.?nam(?!.*south)|democratic.republic.of.vietnam|socialist.republic.of.viet.?nam|north.viet.?nam|viet.?nam.north'
+]
+sourcevar = ["Democratic Republic of Vietnam", "Algeria"]
+destination = ["Zambia", "Vietnam"]
+
+replace_regex(sourcevar, origin, ["Zambia", "Vietnam"])
+
+
+origin_compiled = [re.compile(x, flags=re.IGNORECASE) for x in origin]
+origin_replaced = [get_first_match(x, sourcevar) for x in origin_compiled]
+out = replace_exact(origin, origin_replaced, destination)
+
+replace_regex(sourcevar, origin, ["Zambia", "Vietnam"])
+
+origin_compiled = [re.compile(x, flags=re.IGNORECASE) for x in origin]
+origin_replaced = [get_first_match(x, sourcevar) for x in o]
+out = replace_exact(origin, origin_replaced, destination)
+
+re.search(origin[1], "Democratic Republic of Vietnam")
