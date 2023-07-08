74776cec1c9c8cfcca6ca864072a6c6d709f63c7
Francis R. Kovacs <frankkovacs8@gmail.com>
2023-07-08 15:19:12 -0400

    update formatting

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,18 +1,23 @@
 import pytest
 from countrycode import countrycode
 
+
 def test_numeric():
     assert countrycode("CAN", "iso3c", "iso3n") == 124
 
+
 def test_basic_conversions():
     def name_of(iso3c_code):
-        return countrycode(iso3c_code, origin = 'iso3c', destination = 'country.name')
+        return countrycode(iso3c_code, origin='iso3c', destination='country.name')
+
     assert name_of('CAN') == 'Canada'
     assert name_of(['USA', 'CAN']) == ['United States', 'Canada']
+
 
 def test_issue_252():
     assert countrycode("United States", "country.name", "p5c") == "USA"
     assert countrycode("United States", "country.name", "p5n") == 2
+
 
 def test_invalid_iso3c_to_country_name():
     def name_of(iso3c_code):
@@ -20,5 +25,3 @@ def test_invalid_iso3c_to_country_name():
 
     assert name_of('BAD') is None
     assert name_of(['BAD', 'BLA', 'CAN']) == [None, None, 'Canada']
-
-
