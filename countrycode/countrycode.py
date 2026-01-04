091921cba0e3b5c1c4c8b7da06c71e199b47b68d
Melchior Reihlen <m.reihlenboergers@gmail.com>
2026-01-04 11:02:08 +0100

    added docs and function to include custom dictionary

--- countrycode/countrycode.py
+++ countrycode/countrycode.py
@@ -12,14 +12,34 @@ except ImportError:
     pd = None
 
 pkg_dir, pkg_filename = os.path.split(__file__)
-data_path = os.path.join(pkg_dir, "data", "codelist.pickle")
 
-with open(data_path, "rb") as f:
-    codelist = pickle.load(f)
+def load_dict(data_path: str = os.path.join(pkg_dir, 'data', 'codelist.pickle')):
+    """
+    Load a pickle file containing the countrycode translations.
+    
+    Parameters:
+    data_path (str):
+        The path to a `.pickle` file to be used for matching codes.
+
+    """
+
+    fileextension = os.path.splitext(data_path)[1]
+
+    if fileextension != '.pickle':
+        raise NotImplementedError(f'Custom dicts with `{fileextension}` are not implemented yet. Please use a `.pickle` file or None (to use the default).')
+
+    try:
+        with open(data_path, 'rb') as f:
+            codelist = pickle.load(f)
+    
+    except:
+        raise FileNotFoundError(f'Could not find file at `{data_path}`. Please make sure the file exists at the provided path.')
+    
+    return codelist
 
 
 def countrycode(
-    sourcevar=["DZA", "CAN"], origin="iso3c", destination="country.name.en"
+    sourcevar=["DZA", "CAN"], origin="iso3c", destination="country.name.en", custom_dict = None 
 ):
     """
     Convert country codes or names from one format to another.
@@ -34,6 +54,8 @@ def countrycode(
         The format of the input country codes or names. Default is 'iso3c'.
     destination (str, optional):
         The desired format of the output country codes or names. Default is 'country.name.en'.
+    custom_dict (str, dict, optional):
+        A custom dictionary to be used. Can either be a path to an existing `.pickle` file or a dictionary. Default is None, i.e. the default dict is used. 
 
     Returns:
     list, str, or polars.series.series.Series:
@@ -46,6 +68,11 @@ def countrycode(
     ValueError:
         If the `origin` or `destination` format is not one of the supported formats.
         If the input `sourcevar` is not a string, list, or Polars Series.
+    NotImplementedError:
+        If the custom_file is a string (path to dict file) but not a `.pickle` file.
+        If the custom_dict argument is neither str, None or dict.
+    FileNotFoundError:
+        If the custom_file is a string (path to dict file) and a pickle file but does not exist. 
 
     Example:
     >>> countrycode(['DZA', 'CAN'], origin='iso3c', destination='country.name.en')
@@ -55,14 +82,31 @@ def countrycode(
     This function uses two helper functions (`replace_regex` and `replace_exact`) to perform the actual conversion.
     """
 
-    # user convenience shortcuts
+    # Load the codelist dict.
+    if isinstance(custom_dict, str):
+        # If the custom dict argument is a string it will be interpreted as a path to load 
+        # the custom dict from.
+        codelist = load_dict(custom_dict)
+    
+    elif isinstance(custom_dict, dict):
+        codelist = custom_dict
+
+    elif custom_dict == None:
+        # Default data path is just data/codelist.pickle
+        codelist = load_dict()
+
+    else:
+        raise NotImplementedError(f'The custom_dict has to be either None (to use the default dict), dict or str NOT {type(custom_dict)}')
+
+
+    # user convenience shortcuts only for default dict
     if origin == "country.name":
         origin = "country.name.en.regex"
 
     if origin in [
         "country.name.en",
         "country.name.fr",
-        "country.name.de",
+        "country.name.de", 
         "country.name.it",
     ]:
         origin = origin + ".regex"
@@ -136,9 +180,9 @@ def countrycode(
         "country.name.de.regex",
         "country.name.it.regex",
     ]:
-        out = replace_regex(sourcevar_series, origin, destination)
+        out = replace_regex(sourcevar_series, origin, destination, codelist)
     else:
-        out = replace_exact(sourcevar_series, origin, destination)
+        out = replace_exact(sourcevar_series, origin, destination, codelist)
 
     # output type
     if isinstance(sourcevar, (str, int)):
@@ -159,7 +203,7 @@ def get_first_match(pattern, string_list):
     return None
 
 
-def replace_exact(sourcevar, origin, destination):
+def replace_exact(sourcevar, origin, destination, codelist):
     out = []
     for string in sourcevar:
         match_found = False
@@ -181,7 +225,7 @@ def replace_exact(sourcevar, origin, destination):
     return out
 
 
-def replace_regex(sourcevar, origin, destination):
+def replace_regex(sourcevar, origin, destination, codelist):
     sourcevar_unique = list(set(sourcevar))
     o = []
     d = []
